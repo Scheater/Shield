3e73bf7003233832ad4ecf152002d3f2dd49aef5
Myass:) <141315107+Scheater@users.noreply.github.com>
2025-04-19 14:34:43 +0200

    Update script.js
    Removed Weil unötig

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// JS for start page (index.html)
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("✅ Shield Cheats loaded.");
-  
-  // Optional: focus the Login button on page load
-  const loginBtn = document.querySelector(".btn");
-  if (loginBtn) {
-    loginBtn.focus();
-  }
-
-  // Optional: display date or dynamic content later here
-});
